6a2ecffb7c1b79f3d28e63e66005452c97465732
nathanli11 <nathanliesse@outlook.fr>
2025-11-23 09:34:14 +0100

    classe Grecs plus test unitaire

--- Best_Worst_Of_Options/Grecs.cs
+++ Best_Worst_Of_Options/Grecs.cs
@@ -1,6 +1,49 @@
 namespace Best_Worst_Of_Options;
 
-public class Grecs
+public static class Grecs
 {
-    
+    /// <summary>
+    /// Calcule le vecteur Delta pour une option multi-sous-jacents.
+    /// en utilisant la méthode "bump and revalue"
+    /// </summary>
+    public static double[] Delta(Option option, Market market, double riskFreeRate,
+        double h = 0.01, int numPaths = 5000, int days=252)
+    {
+        // Récupération du nombre de sous jacent de l option
+        int n = option.Underlyings.Count;
+        // Creation du vecteur resultat des deltas
+        double [] deltas = new double[n];
+
+        // Recuperation des spots initiaux
+        double [] S0 = option.Underlyings
+                        .Select(s => s.HistoricalPrices.Values.Last())
+                        .ToArray();
+        
+        // Pour chaque sous jacent
+        for (int i=0; i < n; i++)
+        {
+            string ticker = option.Underlyings[i].Ticker;
+            var stock = option.Underlyings[i];
+
+            // Sauvegarde du spot price
+            double original = S0[i];
+            DateTime lastDate = stock.HistoricalPrices.Keys.Max();
+
+            // Bump up
+            stock.HistoricalPrices[lastDate] = original * (1+h);
+            double VPlus = option.PriceOption(market, riskFreeRate, numPaths, days); // On suppose que la fonction price est celle la
+
+            // Bump down
+            stock.HistoricalPrices[lastDate] = original * (1 - h);
+            double VMinus = option.PriceOption(market, riskFreeRate, numPaths, days);
+
+            // Restauration du spot price
+            stock.HistoricalPrices[lastDate] = original;
+
+            // Calcul du delta
+            deltas[i] = (VPlus - VMinus) / (2.0 * h * original);
+        }
+        return deltas;
+    }
+
 }
---
+++ Test_Best_Worst_Of/TestMarket.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_Best_Worst_Of;
+
+[TestClass]
+public sealed class TestMarket
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
